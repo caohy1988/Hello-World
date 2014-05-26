5681933c40f13de0c6006974a8bd780ae569372f
zhouy <zhouy@zhouydeMacBook-Air.local>
2014-05-26 18:10:55 +0800

    Initial Commit

---
+++ Hello World/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Hello World
+//
+//  Created by zhouy on 14-5-26.
+//  Copyright (c) 2014年 heheheh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Hello World/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Hello World
+//
+//  Created by zhouy on 14-5-26.
+//  Copyright (c) 2014年 heheheh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
